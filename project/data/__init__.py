218564b856d15af3ac4ddc149ec5702db5db1bf4
LeCongThuong <thuongle276@gmail.com>
2021-03-30 16:51:19 +0700

    fix error: import module

--- project/data/__init__.py
+++ project/data/__init__.py
@@ -1,3 +1,3 @@
 from .mnist import MNISTDataModule
 from .cifar import CIFARDataModule
-from .adaptive_resizer_batch import AdaptiveResizerDataModule, ShopeeDatasetLoader
+from .adaptive_resizer_batch import AdaptiveResizerDataset, ShopeeDatasetLoader
