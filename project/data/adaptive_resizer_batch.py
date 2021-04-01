4c7b786dbb6e92900e527d353be2f0bd9c811ec3
LeCongThuong <thuongle276@gmail.com>
2021-04-01 14:54:03 +0700

    fix transform images

--- project/data/adaptive_resizer_batch.py
+++ project/data/adaptive_resizer_batch.py
@@ -36,8 +36,7 @@ class AdaptiveResizerDataset(Dataset):
         image = cv2.imread(image_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         # transform data
-        if self.do_train:
-            image = self.transform(image)["image"]
+        image = self.transform(image)["image"]
         image = np.transpose(image, (2, 0, 1))
         title = self.text_process(title)
 
