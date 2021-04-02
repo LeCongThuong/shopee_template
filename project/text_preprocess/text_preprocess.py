094993520c0e5efaab70563566858f80d3f74eb6
LeCongThuong <thuongle276@gmail.com>
2021-04-03 00:00:03 +0700

    fix preprocess text

--- project/text_preprocess/text_preprocess.py
+++ project/text_preprocess/text_preprocess.py
@@ -1,6 +1,7 @@
 import string
 import nltk
 nltk.download('punkt')
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer, WordNetLemmatizer
