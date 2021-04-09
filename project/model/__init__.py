23baddc11ef7fdbaeeb9d09270022ec1afdf7aee
LeCongThuong <thuongle276@gmail.com>
2021-04-09 17:09:56 +0700

    add bert config

--- project/model/__init__.py
+++ project/model/__init__.py
@@ -1,3 +1,4 @@
 from .baseline_efficientnet_bert import BaselineModel, BaseModel
 from .efficientnet import EfficientnetExtractor
 from .efficientnet_resizer import ResizeEfficientnet
+from .bert_module import BertBaseCaseModel
