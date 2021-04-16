adcf21fc1688218418813ac286242378404fbcac
LeCongThuong <thuongle276@gmail.com>
2021-04-16 18:30:30 +0700

    add scale aug feature

--- project/augmentation/image_torch_transform.py
+++ project/augmentation/image_torch_transform.py
@@ -2,12 +2,15 @@ import torchvision.transforms as transforms
 
 
 class TorchAug:
-    def __init__(self, height=512, width=512):
-        self.transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.RandomResizedCrop(size=(height, width)),
-            transforms.RandomHorizontalFlip(0.5),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+    def __init__(self, height=512, width=512, gray_image=False, gray_prob=0.3):
+        image_transform = [transforms.ToTensor(),
+                           transforms.RandomResizedCrop(size=(height, width)),
+                           transforms.RandomHorizontalFlip(0.5)]
+        if gray_image:
+            image_transform.append(transforms.RandomGrayscale(p=gray_prob))
+        image_transform.append(transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]))
+
+        self.transform = transforms.Compose(image_transform)
 
     def __call__(self, image):
         return self.transform(image)
