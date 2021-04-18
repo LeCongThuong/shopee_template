c2d08d163c2b835e95be68205448571511ef946f
LeCongThuong <thuongle276@gmail.com>
2021-04-18 20:45:08 +0700

    add gray feature

--- project/augmentation/image_torch_transform.py
+++ project/augmentation/image_torch_transform.py
@@ -2,13 +2,15 @@ import torchvision.transforms as transforms
 
 
 class TorchAug:
-    def __init__(self, height=512, width=512, gray_image=False, gray_prob=0.3):
+    def __init__(self, height=512, width=512, gray_image=False, gray_prob=1):
         image_transform = [transforms.ToTensor(),
                            transforms.RandomResizedCrop(size=(height, width)),
                            transforms.RandomHorizontalFlip(0.5)]
         if gray_image:
             image_transform.append(transforms.RandomGrayscale(p=gray_prob))
-        image_transform.append(transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]))
+            image_transform.append(transforms.Normalize(mean=[0.45, 0.45, 0.45], std=[0.22, 0.22, 0.22]))
+        else:
+            image_transform.append(transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]))
 
         self.transform = transforms.Compose(image_transform)
 
--- project/augmentation/image_transform_default.py
+++ project/augmentation/image_transform_default.py
@@ -2,11 +2,17 @@ import torchvision.transforms as transforms
 
 
 class DefaultAug:
-    def __init__(self, height=512, width=512):
-        self.transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Resize(size=(height, width)),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+    def __init__(self, height=512, width=512, gray_image=False, gray_prob=1):
+        image_transform = [transforms.ToTensor(),
+                           transforms.Resize(size=(height, width))
+                           ]
+        if gray_image:
+            image_transform.append(transforms.RandomGrayscale(p=gray_prob))
+            image_transform.append(transforms.Normalize(mean=[0.45, 0.45, 0.45], std=[0.22, 0.22, 0.22]))
+        else:
+            image_transform.append(transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]))
+
+        self.transform = transforms.Compose(image_transform)
 
     def __call__(self, image):
         return self.transform(image)
