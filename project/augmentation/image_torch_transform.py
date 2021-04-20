8faef491fe8f659362a2e37c5fc6e072fd13899c
LeCongThuong <thuongle276@gmail.com>
2021-04-20 23:19:14 +0700

    add eraser feat

--- project/augmentation/image_torch_transform.py
+++ project/augmentation/image_torch_transform.py
@@ -2,10 +2,12 @@ import torchvision.transforms as transforms
 
 
 class TorchAug:
-    def __init__(self, height=512, width=512, gray_image=False, gray_prob=1):
+    def __init__(self, height=512, width=512, gray_image=False, gray_prob=1, eraser_aug=False, eraser_prob=0.3):
         image_transform = [transforms.ToTensor(),
                            transforms.RandomResizedCrop(size=(height, width)),
                            transforms.RandomHorizontalFlip(0.5)]
+        if eraser_aug:
+            image_transform.append(transforms.RandomErasing(p=eraser_prob))
         if gray_image:
             image_transform.append(transforms.RandomGrayscale(p=gray_prob))
             image_transform.append(transforms.Normalize(mean=[0.45, 0.45, 0.45], std=[0.22, 0.22, 0.22]))
